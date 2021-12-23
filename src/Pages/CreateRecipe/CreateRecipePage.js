1161f5e67eda7c3450466e307ce7f33fef0f29c7
Mihail Alekseev <mihailalekseev02@gmail.com>
2021-12-23 15:52:03 +0200

    Done CreateRecipePage, unfortunattly with monolith component.

--- src/Pages/CreateRecipe/CreateRecipePage.js
+++ src/Pages/CreateRecipe/CreateRecipePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect, useRef } from "react";
+import React, { useState, useContext, useEffect, useRef, useCallback } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UrlContext } from "~/App";
 import useValueSaver from "~/CustomHooks/useValueSaver";
@@ -20,77 +20,127 @@ const initialState = {
 	steps: "",
 };
 
-const Select = (props) => {
-	const [options, setOptions] = useState();
-
-	useEffect(async () => await sendRequest(null, props.apiRequestUrl, "GET").then(setOptions).catch(console.log), []);
-
-	const adding = props.adding ? props.adding : "";
-
-	return options ? (
-		<select value={props.value} onChange={props.onChange} name={props.name}>
-			{options.map((option) => (
-				<option value={option.id} key={option.id}>
-					{option.name + adding}
-				</option>
-			))}
-		</select>
-	) : null;
-};
-
 const CreateForm = () => {
 	const navigate = useNavigate();
 	const [recipe, setRecipeValue] = useValueSaver(initialState);
 	const [token, setToken] = useToken();
 	const apiRootUrl = useContext(UrlContext);
-	const ref = useRef();
+	const [selectedFile, setSelectedFile] = useState();
 
-	const onSubmitHandle = async (event) => {
+	const [categoryOptions, setCategoryOptions] = useState();
+	const [cuisineOptions, setCuisineOptions] = useState();
+	useEffect(
+		async () =>
+			await sendRequest(null, apiRootUrl + "categories", "GET")
+				.then(setCategoryOptions)
+				.catch(console.log),
+		[]
+	);
+	useEffect(
+		async () =>
+			await sendRequest(null, apiRootUrl + "cuisines", "GET")
+				.then(setCuisineOptions)
+				.catch(console.log),
+		[]
+	);
+
+	const fileUploadedHandler = (event) => setSelectedFile(event.target.files[0]);
+
+	const supportedTypes = ["image/jpeg", "image/jpg", "image/png"];
+
+	const onSubmitHandle = (event) => {
 		event.preventDefault();
-
-		console.log(recipe);
-		console.log(ref);
-		/*
-		await sendRequest(
-			{
+		const reader = new FileReader();
+		reader.readAsDataURL(selectedFile);
+		reader.onload = async () => {
+			const image = reader.result.substring(reader.result.indexOf(",") + 1);
+			console.log({
 				name: recipe.name,
-				cookingTime: recipe.cookingTime,
-				calories: recipe.calories,
-				proteins: recipe.proteins,
-				fats: recipe.fats,
-				carbs: recipe.carbs,
-				categoryId: recipe.categoryId,
-				cuisineId: recipe.cuisineId,
+				image: image,
+				cookingTime: parseInt(recipe.cookingTime),
+				calories: parseInt(recipe.calories),
+				proteins: parseInt(recipe.proteins),
+				fats: parseInt(recipe.fats),
+				carbs: parseInt(recipe.carbs),
+				categoryId: parseInt(recipe.categoryId),
+				cuisineId: parseInt(recipe.cuisineId),
 				description: recipe.description,
 				steps: recipe.steps,
-				authorId: token.id,
-			},
-			apiRootUrl + "recipes/new",
-			"POST"
-		)
-			.then((response) => {
-				setToken(response);
-				navigate("/recipes/" + response.id);
-			})
-			.catch(console.log);*/
+				authorId: parseInt(token.id),
+			});
+			await sendRequest(
+				{
+					name: recipe.name,
+					image: image,
+					cookingTime: parseInt(recipe.cookingTime),
+					calories: parseInt(recipe.calories),
+					proteins: parseInt(recipe.proteins),
+					fats: parseInt(recipe.fats),
+					carbs: parseInt(recipe.carbs),
+					categoryId: parseInt(recipe.categoryId),
+					cuisineId: parseInt(recipe.cuisineId),
+					description: recipe.description,
+					steps: recipe.steps,
+					authorId: parseInt(token.id),
+				},
+				apiRootUrl + "recipes",
+				"POST"
+			)
+				.then((response) => {
+					navigate("/recipes/" + response.id);
+				})
+				.catch(console.log);
+		};
+		reader.onerror = console.log;
 	};
 
 	return (
 		<form className="form-container" onSubmit={onSubmitHandle}>
 			<div className="container">
 				<input value={recipe.name} onChange={setRecipeValue} name="name" type="text" placeholder="Название рецепта" required />
-				<input value={recipe.cookingTime} onChange={setRecipeValue} name="cookingTime" type="text" placeholder="Время приготовления" maxlength="3" required />
+				<input value={recipe.cookingTime} onChange={setRecipeValue} name="cookingTime" type="text" placeholder="Время приготовления" maxLength="3" required />
 			</div>
 			<div className="params-container">
-				<input value={recipe.calories} onChange={setRecipeValue} name="calories" type="text" placeholder="Калории" maxlength="5" />
-				<input value={recipe.proteins} onChange={setRecipeValue} name="proteins" type="text" placeholder="Протеины" maxlength="5" />
-				<input value={recipe.fats} onChange={setRecipeValue} name="fats" type="text" placeholder="Жиры" maxlength="5" />
-				<input value={recipe.carbs} onChange={setRecipeValue} name="carbs" type="text" placeholder="Углеводы" maxlength="5" />
+				<input value={recipe.calories} onChange={setRecipeValue} name="calories" type="text" placeholder="Калории" maxLength="5" />
+				<input value={recipe.proteins} onChange={setRecipeValue} name="proteins" type="text" placeholder="Протеины" maxLength="5" />
+				<input value={recipe.fats} onChange={setRecipeValue} name="fats" type="text" placeholder="Жиры" maxLength="5" />
+				<input value={recipe.carbs} onChange={setRecipeValue} name="carbs" type="text" placeholder="Углеводы" maxLength="5" />
 			</div>
 			<div className="container">
-				<Select apiRequestUrl={apiRootUrl + "categories"} value={recipe.categoryId} onChange={setRecipeValue} name="category" />
-				<Select apiRequestUrl={apiRootUrl + "cuisines"} adding=" кухня" value={recipe.cuisineId} onChange={setRecipeValue} name="cuisine" />
+				{categoryOptions ? (
+					<select onChange={setRecipeValue} name="categoryId">
+						{categoryOptions.map((option) => (
+							<option value={option.id} key={option.id}>
+								{option.name}
+							</option>
+						))}
+					</select>
+				) : null}
+
+				{cuisineOptions ? (
+					<select onChange={setRecipeValue} name="cuisineId">
+						{cuisineOptions.map((option) => (
+							<option value={option.id} key={option.id}>
+								{option.name + " кухня"}
+							</option>
+						))}
+					</select>
+				) : null}
 			</div>
+
+			<label className="upload-label container" htmlFor="upload-photo">
+				<input id="upload-photo" type="file" name="file" onChange={fileUploadedHandler} required />
+				<div className="upload-img"></div>
+				<span>{!selectedFile ? "Добавить фото (Загрузить)" : "Фото загружено"}</span>
+				{selectedFile && supportedTypes.includes(selectedFile.type) ? (
+					<>
+						<span>Название файла: {selectedFile.name}</span>
+						<span>Размер: {Math.round((selectedFile.size / 1024 / 1024 + Number.EPSILON) * 100) / 100} Mb</span>
+					</>
+				) : selectedFile ? (
+					<span>Поддерживаются фотографии только {supportedTypes.map((t) => t.split("/")[1] + " ")}форматов</span>
+				) : null}
+			</label>
 
 			<textarea value={recipe.description} onChange={setRecipeValue} name="description" type="text" placeholder="Описание" autoComplete="on" className="description" />
 			<textarea value={recipe.steps} onChange={setRecipeValue} name="steps" type="text" placeholder="Пошаговая инструкция" autoComplete="on" className="description" />
